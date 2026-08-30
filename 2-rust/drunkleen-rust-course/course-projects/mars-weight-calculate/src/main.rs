c94efe86d39519a91c17038d6b422300a38aef04
antolios-altarino <music80senpai@gmail.com>
2026-08-30 05:08:31 -0700

    practice on the if and match conditions

--- 2-rust/drunkleen-rust-course/course-projects/mars-weight-calculate/src/main.rs
+++ 2-rust/drunkleen-rust-course/course-projects/mars-weight-calculate/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let calc:f64 = mars_weight_calculater(51.0);
-    println!("{:?}",calc);
+    let calc: f64 = mars_weight_calculator(51.0);
+    println!("{:?}" , calc);
 }
 
-fn mars_weight_calculater(weight:f64) -> f64 {
+fn mars_weight_calculator(weight:f64) -> f64 {
     weight * 0.378
 }
--- 2-rust/drunkleen-rust-course/course-projects/rock-paper-scissors/src/main.rs
+++ 2-rust/drunkleen-rust-course/course-projects/rock-paper-scissors/src/main.rs
@@ -1,21 +1,104 @@
-use random_number::random;
+use rand::random_range;
 
 fn main() {
-    let enemy_choice: u8 = Edit Prediction!(0..4);
-    let my_choice: u8 = random!(0..4);
-    println!("{} {}", my_choice, enemy_choice);
+    rock_paper_scissors_if(2);
+    rock_paper_scissors_match(1);
 }
-fn rps() -> u8 {
-    let enemy_choice: u8 = random!(0..4);
-    let my_choice: u8 = random!(0..4);
-    if enemy_choice == 1 {
-        println!("rock")
-    }
-    if enemy_choice == 2 {
 
+/* Rock Paper Scissors game with if condition*/
+fn rock_paper_scissors_if(my_choice: u8) -> u8 {
+    println!("Welcome to rock paper scissors");
+    println!("1. rock");
+    println!("2. paper");
+    println!("3. scissors");
+    
+    let enemy_choice: u8 = random_range(1..4);
+    
+    if my_choice == 1 {
+        println!("your choice is: rock");
+    } else if my_choice == 2 {
+        println!("your choice is: paper");
+    } else {
+        println!("your choice is: scissors");
+    }
+
+    if enemy_choice == 1 {
+        println!("enemy choice is: rock");
+    } else if enemy_choice == 2 {
+        println!("enemy choice is: paper");
+    } else {
+        println!("enemy choice is: scissors");
+    }
+
+    if my_choice == enemy_choice {
+        println!("draw");
+        1
+    } else if my_choice == 1 && enemy_choice == 2 {
+        println!("you lose");
+        2
+    } else if my_choice == 1 && enemy_choice == 3 {
+        println!("you win");
+        1
+    } else if my_choice == 2 && enemy_choice == 1 {
+        println!("you win");
+        1
+    } else if my_choice == 2 && enemy_choice == 3 {
+        println!("you lose");
+        2
+    } else if my_choice == 3 && enemy_choice == 1 {
+        println!("you lose");
+        2
+    } else if my_choice == 3 && enemy_choice == 2 {
+        println!("you win");
+        1
+    } else {
+        println!("error");
+        4
+    }
+    
+}
+
+/* Rock Paper Scissors game with match condition*/
+fn rock_paper_scissors_match(my_choice: u8) -> u8 {
+    println!("Welcome to rock paper scissors");
+    println!("1. rock");
+    println!("2. paper");
+    println!("3. scissors");
+    
+    let enemy_choice: u8 = random_range(1..4);
+    
+    match my_choice {
+        1 => println!("your choice is: rock"),
+        2 => println!("your choice is: paper"),
+        3 => println!("your choice is: scissors"),
+        _ => println!("invalid choice"),
+    }
+
+    match enemy_choice {
+        1 => println!("enemy choice is: rock"),
+        2 => println!("enemy choice is: paper"),
+        3 => println!("enemy choice is: scissors"),
+        _ => println!("invalid choice"),
+    }
+
+    match (my_choice, enemy_choice) {
+        (x, y) if x == y => {
+            println!("draw");
+            1
+        },
+        (1, 2) | (2, 3) | (3, 1) => {
+            println!("you lose");
+            2
+        },
+        (1, 3) | (2, 1) | (3, 2) => {
+            println!("you win");
+            1
+        },
+        _ => {
+            println!("error");
+            4
+        }
     }
 }
-/*
-fn scissors_rock_paper(choice:&str) -> str {
-}
- */
+
+
